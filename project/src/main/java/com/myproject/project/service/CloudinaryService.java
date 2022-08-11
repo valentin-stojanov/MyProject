66766aa538432966530bfb13740f178366d90cf4
valentin-stojanov <valentin_stojanov@abv.bg>
2022-08-11 17:08:34 +0300

    Working on picture upload.

--- project/src/main/java/com/myproject/project/service/CloudinaryService.java
+++ project/src/main/java/com/myproject/project/service/CloudinaryService.java
@@ -1,17 +1,56 @@
 package com.myproject.project.service;
 
+import com.cloudinary.Cloudinary;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
 
 @Service
 public class CloudinaryService {
 
-    public CloudinaryImage upload(MultipartFile file){
+    private static final String TEMP_FILE = "temp-file";
+    private static final String URL = "url";
+    private static final String PUBLIC_ID = "public-id";
+    private final Cloudinary cloudinary;
 
-        return null;
+    public CloudinaryService(Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
     }
 
-    public boolean delete(String publicId){
-        return false;
+    public CloudinaryImage upload(MultipartFile multipartFile) throws IOException {
+
+        File tempFile = File.createTempFile(TEMP_FILE, multipartFile.getOriginalFilename());
+        multipartFile.transferTo(tempFile);
+
+        CloudinaryImage result;
+        try {
+            @SuppressWarnings("unchecked")
+            Map<String, String> uploadResult = this.cloudinary
+                    .uploader()
+                    .upload(tempFile, Map.of());
+
+            String url = uploadResult.getOrDefault(URL,
+                    "https://previews.123rf.com/images/olejio/olejio1802/olejio180200041/95585020-funny-design-404-page-not-found-vector-illustration-geek-with-metal-detector-searching-the-big-data.jpg");
+            String publicId = uploadResult.getOrDefault(PUBLIC_ID, "");
+            result = new CloudinaryImage()
+                    .setPublicId(publicId)
+                    .setUrl(url);
+        } finally {
+            tempFile.delete();
+        }
+
+        return result;
+    }
+
+    public boolean delete(String publicId) {
+        try {
+            this.cloudinary.uploader().destroy(publicId, Map.of());
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
     }
 }
--- project/src/main/java/com/myproject/project/web/RouteController.java
+++ project/src/main/java/com/myproject/project/web/RouteController.java
@@ -1,13 +1,30 @@
 package com.myproject.project.web;
 
 import com.myproject.project.model.dto.PictureUploadDto;
+import com.myproject.project.model.entity.PictureEntity;
+import com.myproject.project.repository.PictureRepository;
+import com.myproject.project.service.CloudinaryImage;
+import com.myproject.project.service.CloudinaryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 public class RouteController {
+
+    private final CloudinaryService cloudinaryService;
+    private final PictureRepository pictureRepository;
+
+    public RouteController(CloudinaryService cloudinaryService,
+                           PictureRepository pictureRepository) {
+        this.cloudinaryService = cloudinaryService;
+        //TODO: refactoring!
+        this.pictureRepository = pictureRepository;
+    }
 
     @GetMapping("/route")
     public String addRoute() {
@@ -15,12 +32,25 @@ public class RouteController {
     }
 
     @PostMapping("/route")
-    public String addPicture(PictureUploadDto pictureUploadDto) {
+    public String addPicture(PictureUploadDto pictureUploadDto) throws IOException {
+        PictureEntity picture = createPictureEntity(pictureUploadDto.getPicture(), pictureUploadDto.getTitle());
+
+        this.pictureRepository.save(picture);
+
         return "redirect:/pictures/all";
+    }
+
+    private PictureEntity createPictureEntity(MultipartFile multipartFile, String title) throws IOException {
+        CloudinaryImage uploaded = this.cloudinaryService.upload(multipartFile);
+        return new PictureEntity()
+                .setPublicId(uploaded.getPublicId())
+                .setTitle(title)
+                .setUrl(uploaded.getUrl());
     }
 
     @GetMapping("/pictures/all")
     public String allPictures(Model model) {
+
         return "all";
     }
 }
