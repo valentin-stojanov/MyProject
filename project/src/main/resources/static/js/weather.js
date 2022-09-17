e886ef6eb552ab736392295b42d01d259b8948c8
Valentin <valentin_stojanov@abv.bg>
2022-09-17 21:09:09 +0300

    Added weather.js.

---
+++ project/src/main/resources/static/js/weather.js
@@ -0,0 +1,13 @@
+const boxImgA = document.getElementById('box-a-img');
+const boxImgB = document.getElementById('box-b-img');
+const boxTempA = document.getElementById('box-a-temp');
+const boxTemB = document.getElementById('box-b-temp');
+
+fetch("http://api.openweathermap.org/data/2.5/weather?q=Sofia&appid=8dd1b8c6c70655b59ef4f75b4d9fb753")
+.then(data => data.json())
+.then(info =>{
+    console.log(info)
+
+    boxTempA.innerText = Math.round(info.main.temp - 273.15);
+    boxImgA.src = '/images/weather-icons/' + info.weather[0].icon + '.png';
+})
