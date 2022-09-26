1d3293a018c317858b72e96e584ba39a031525e7
valentin-stojanov <valentin_stojanov@abv.bg>
2022-09-26 13:55:10 +0300

    Working comments.

---
+++ project/src/main/java/com/myproject/project/repository/CommentsRepository.java
@@ -0,0 +1,7 @@
+package com.myproject.project.repository;
+
+import com.myproject.project.model.entity.CommentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentsRepository extends JpaRepository<CommentEntity> {
+}
