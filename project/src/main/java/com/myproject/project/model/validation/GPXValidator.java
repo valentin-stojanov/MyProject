aafea91716258f4522739d9f3d4e361776a2aff0
valentin-stojanov <valentin_stojanov@abv.bg>
2022-12-13 16:26:19 +0200

    Added gpx.xsd; working on gpx validation.

---
+++ project/src/main/java/com/myproject/project/model/validation/GPXValidator.java
@@ -0,0 +1,29 @@
+package com.myproject.project.model.validation;
+
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import static org.springframework.util.ResourceUtils.getFile;
+
+public class GPXValidator {
+
+    private Validator initValidator(String xsdPath) throws SAXException, FileNotFoundException {
+        SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Source schemaFile = new StreamSource(getFile(xsdPath));
+        Schema schema = factory.newSchema(schemaFile);
+        return schema.newValidator();
+    }
+
+    private File getFile(String location) {
+        return new File(getClass().getClassLoader().getResource(location).getFile());
+    }
+}
