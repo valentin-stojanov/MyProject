98a89c84c250921abb191b446c589c5a6de2dbf9
valentin-stojanov <valentin_stojanov@abv.bg>
2022-12-20 17:14:11 +0200

    Added testRegistration() in AppUserDetailsServiceTest.

--- project/src/test/java/com/myproject/project/web/UserRegistrationControllerIntegrationTest.java
+++ project/src/test/java/com/myproject/project/web/UserRegistrationControllerIntegrationTest.java
@@ -7,9 +7,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -23,5 +24,22 @@ class UserRegistrationControllerIntegrationTest {
         mockMvc.perform(get("/users/register"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("register"));
+    }
+
+    @Test
+    void testRegistration() throws Exception {
+        mockMvc.perform(post("http://localhost/users/register")
+                        .param("email", "example@exam.cam")
+                        .param("firstName", "Test")
+                        .param("lastName", "Testov")
+                        .param("age", "18")
+                        .param("password", "topsecret")
+                        .param("confirmPassword", "topsecret")
+                        .with(csrf())
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+
+
     }
 }
