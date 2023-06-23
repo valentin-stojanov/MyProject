e7a98032f3915fa9a62e33bc252912b91e24645d
valentin <valentin.h.stoyanov@gmail.com>
2023-06-23 23:11:27 +0300

    Some work on UserController

---
+++ project/src/main/java/com/myproject/project/model/dto/UserResetEmailDto.java
@@ -0,0 +1,4 @@
+package com.myproject.project.model.dto;
+
+public class UserResetEmailDto {
+}
--- project/src/main/java/com/myproject/project/web/UserController.java
+++ project/src/main/java/com/myproject/project/web/UserController.java
@@ -1,5 +1,6 @@
 package com.myproject.project.web;
 
+import com.myproject.project.model.dto.UserResetEmailDto;
 import com.myproject.project.model.dto.UserViewModel;
 import com.myproject.project.service.UserService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -7,17 +8,25 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.naming.Binding;
+import javax.validation.Valid;
+
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
+    @ModelAttribute("userResetEmailDto")
+    public UserResetEmailDto initUserResetEmailModel(){
+        return new UserResetEmailDto();
+    }
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -34,6 +43,18 @@ public class UserController {
         redirectAttributes.
                 addFlashAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY, username)
                 .addFlashAttribute("bad_credentials", true);
+        return "redirect:/users/login";
+    }
+
+    @PostMapping("/reset-password")
+    public String resetPassword(@Valid UserResetEmailDto userResetEmailDto,
+                                BindingResult bindingResult,
+                                RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()){
+
+        }
+
         return "redirect:/users/login";
     }
 
