87fae19f2232cbf05af085df40ba04c76fef8ff0
valentin-stojanov <valentin_stojanov@abv.bg>
2023-08-24 14:34:16 +0300

    Weather icon change.

--- project/src/main/resources/static/js/weather.js
+++ project/src/main/resources/static/js/weather.js
@@ -6,5 +6,5 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=Sofia&appid=8485efed455
 .then(info =>{
     // console.log(info)
     boxTempA.innerText = Math.round(info.main.temp - 273.15);
-    boxImgA.src = '/images/weather-icons/' + info.weather[0].icon + '.png';
+    boxImgA.src = 'https://openweathermap.org/img/wn/' + info.weather[0].icon + '@4x.png';
 })
