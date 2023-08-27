b19894e17317c34f8aba0984d6814df22764084c
valentin <valentin.h.stoyanov@gmail.com>
2023-08-27 18:10:52 +0300

    Added 4 days forecast on indexN.html

--- project/src/main/resources/static/js/map.js
+++ project/src/main/resources/static/js/map.js
@@ -1,4 +1,4 @@
-let map = L.map('map').setView([42.76, 25.23], 8);
+let map = L.map('map').setView([42.76, 25.23], 7);
 
 let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
--- project/src/main/resources/static/js/weather.js
+++ project/src/main/resources/static/js/weather.js
@@ -1,24 +1,10 @@
-const boxImgA = document.getElementById('box-a-img');
-const boxTempA = document.getElementById('box-a-temp');
-
-fetch("https://api.openweathermap.org/data/2.5/weather?q=Sofia&appid=8485efed455456abfa98a49fe0c985f2")
-    .then(data => data.json())
-    .then(info => {
-        // console.log(info)
-        boxTempA.innerText = Math.round(info.main.temp - 273.15);
-        boxImgA.src = 'https://openweathermap.org/img/wn/' + info.weather[0].icon + '@4x.png';
-    })
-
-//###########################
-
 function showCurrentWeather(curr){
     let dt = new Date(curr.dt * 1000);
-
     return `<div class="row md-6" id="current-weather" xmlns="http://www.w3.org/1999/html">
                     <div class="col">
-                        <div class="card mb-4">
-                            <div class="card-header">${dt.toDateString()} Summary: ${curr.weather[0].description}</div>
-                            <div class="card-body">
+                        <div class="card text-bg-light mb-3">
+                            <div class="card-header text-center">${dt.toDateString()} Summary: ${curr.weather[0].description}</div>
+                            <div class="card-body text-start">
                                 <div class="row">
                                     <div class="col-md-4">
                                         <img src="http://openweathermap.org/img/wn/${curr.weather[0].icon}@4x.png"
@@ -28,8 +14,8 @@ function showCurrentWeather(curr){
                                     <div class="col-md-8">
                                         <div class="row">
                                             <div class="col-md-6">
-                                                <p>${curr.temp}&deg;C</p>
-                                                <small>Feels like ${curr.feels_like}&deg;C</small>
+                                                <p class="fs-4 fw-normal">${Math.round(curr.temp)}&deg;C</p>
+                                                <small>Feels like ${Math.round(curr.feels_like)}&deg;C</small>
                                             </div>
                                             <div class="col-md-6">
                                                 <small>Humidity ${curr.humidity}%</small> </br>
@@ -55,6 +41,7 @@ function showCurrentWeather(curr){
 
 const app = {
     init: () => {
+        window.addEventListener('load', app.fetchWeather)
         document
             .getElementById('btnGet')
             .addEventListener('click', app.fetchWeather);
@@ -108,52 +95,33 @@ const app = {
     showWeather: (resp) => {
         console.log(resp);
         let currentWeatherElement = document.getElementById("current-weather")
-        let row = document.querySelector('.weather.row');
-        //clear out the old weather and add the new
-        // row.innerHTML = '';
         let curr = resp.current;
+        currentWeatherElement.innerHTML = showCurrentWeather(curr);
 
-         currentWeatherElement.innerHTML = showCurrentWeather(curr);
+        let forecast = document.getElementById('weather-forecast');
+        //clear out the old weather and add the new
+        forecast.innerHTML = '';
 
-        // row.innerHTML = resp.daily
-        //     .map((day, idx) => {
-        //         if (idx <= 3) {
-        //             let dt = new Date(day.dt * 1000); //timestamp * 1000
-        //             let sr = new Date(day.sunrise * 1000).toTimeString();
-        //             let ss = new Date(day.sunset * 1000).toTimeString();
-        //             return `<div class="col">
-        //       <div class="card">
-        //       <h5 class="card-title p-2">${dt.toDateString()}</h5>
-        //         <img
-        //           src="http://openweathermap.org/img/wn/${
-        //                 day.weather[0].icon
-        //             }@4x.png"
-        //           class="card-img-top"
-        //           alt="${day.weather[0].description}"
-        //         />
-        //         <div class="card-body">
-        //           <h3 class="card-title">${day.weather[0].main}</h3>
-        //           <p class="card-text">High ${day.temp.max}&deg;C Low ${
-        //                 day.temp.min
-        //             }&deg;C</p>
-        //           <p class="card-text">High Feels like ${day.feels_like.day}&deg;C</p>
-        //           <p class="card-text">Pressure ${day.pressure}mb</p>
-        //           <p class="card-text">Humidity ${day.humidity}%</p>
-        //           <p class="card-text">UV Index ${day.uvi}</p>
-        //           <p class="card-text">Precipitation ${Math.round(day.pop * 100)}%</p>
-        //           <p class="card-text">Dewpoint ${day.dew_point}</p>
-        //           <p class="card-text">Wind ${day.wind_speed}m/s, ${
-        //                 day.wind_deg
-        //             }&deg;</p>
-        //           <p class="card-text">Sunrise ${sr}</p>
-        //           <p class="card-text">Sunset ${ss}</p>
-        //         </div>
-        //       </div>
-        //     </div>
-        //   </div>`;
-        //         }
-        //     })
-        //     .join(' ');
+        forecast.innerHTML = resp.daily
+            .map((day, idx) => {
+                if (idx <= 3) {
+                    let dt = new Date(day.dt * 1000); //timestamp * 1000
+                    let dateStr = dt.toDateString();
+                    return `<div class="col-md-3 col-sm-6">                        
+                        <div class="card text-bg-light mb-3" style="width: 100%; height: 100%">
+                             <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png"
+                              class="card-img-top" alt="${day.weather[0].description}"
+                              style="height: 5rem;">
+                             <div class="card-body">
+                               <h5 class="card-title">${Math.round(day.temp.day)}&deg;C</h5>
+                               <p class="card-text">${dateStr.slice(0, dateStr.length - 4)}</p>
+                               <small class="fs-6">${day.summary}</small>
+                             </div>
+                        </div>
+                    </div>`;
+                }
+            })
+            .join(' ');
     }
 };
 
