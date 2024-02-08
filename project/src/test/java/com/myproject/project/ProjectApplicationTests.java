548cc7a2473b35ffcfbe8f7d219b5dbf13c8e05b
valentin <valentin.h.stoyanov@gmail.com>
2024-02-09 00:15:42 +0200

    Add testcontainers for integration tests

--- project/src/test/java/com/myproject/project/ProjectApplicationTests.java
+++ project/src/test/java/com/myproject/project/ProjectApplicationTests.java
@@ -2,12 +2,33 @@ package com.myproject.project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class ProjectApplicationTests {
 
+	@Container
+	public static PostgreSQLContainer container = new PostgreSQLContainer()
+			.withUsername("testUser")
+			.withPassword("password")
+			.withDatabaseName("testDB");
+
+	@DynamicPropertySource
+	static void properties(DynamicPropertyRegistry registry){
+		registry.add("spring.datasource.url", container::getJdbcUrl);
+		registry.add("spring.datasource.password", container::getPassword);
+		registry.add("spring.datasource.username", container::getUsername);
+	}
+
 	@Test
 	void contextLoads() {
+		System.out.println("Context loaded");
 	}
 
 }
--- project/src/test/java/com/myproject/project/web/UserRegistrationControllerIntegrationTest.java
+++ project/src/test/java/com/myproject/project/web/UserRegistrationControllerIntegrationTest.java
@@ -1,20 +1,40 @@
 package com.myproject.project.web;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+
+@Testcontainers
 @SpringBootTest
 @AutoConfigureMockMvc
 class UserRegistrationControllerIntegrationTest {
+
+    @Container
+    static PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:15-alpine")
+            .withUsername("testUser")
+            .withPassword("password")
+            .withDatabaseName("testDB");
+
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.password", container::getPassword);
+        registry.add("spring.datasource.username", container::getUsername);
+    }
 
     @Autowired
     private MockMvc mockMvc;
@@ -27,6 +47,7 @@ class UserRegistrationControllerIntegrationTest {
     }
 
     @Test
+    @Sql(value = "/data.sql")
     void testRegistration() throws Exception {
         mockMvc.perform(post("http://localhost/users/register")
                         .param("email", "example@exam.cam")
